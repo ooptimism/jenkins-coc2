8218181caef37322dc4a57d484137c7200796488
xuelm <xue.lm@neusoft.com>
2020-09-23 15:56:37 +0800

    Configure as code

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,10 @@ echo "Genarate JENKINS SSH KEY"
 source /usr/local/bin/generate_key.sh
 echo "machine gerrit.coc-automative.com login gerritadmin password 123" > /var/jenkins_home/.netrc
 cd /var/jenkins_home/
-mkdir -p /var/jenkins_home/tools/hudson.plugins.sonar.SonarRunnerInstallation/MySonarQube
-unzip /sonar-scanner-cli-4.4.0.2170-linux.zip && mv sonar-scanner-4.4.0.2170-linux/* /var/jenkins_home/tools/hudson.plugins.sonar.SonarRunnerInstallation/MySonarQube
+if [ ! -d /var/jenkins_home/tools/hudson.plugins.sonar.SonarRunnerInstallation/MySonarQube ]; then
+    mkdir -p /var/jenkins_home/tools/hudson.plugins.sonar.SonarRunnerInstallation/MySonarQube
+    unzip /sonar-scanner-cli-4.4.0.2170-linux.zip && mv sonar-scanner-4.4.0.2170-linux/* /var/jenkins_home/tools/hudson.plugins.sonar.SonarRunnerInstallation/MySonarQube
+fi
 echo "start JENKINS"
 # if `docker run` first argument start with `--` the user is passing jenkins launcher arguments
 if [[ $# -lt 1 ]] || [[ "$1" == "--"* ]]; then
